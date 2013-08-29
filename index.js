93f9367b3951d2ba6df17a30a8dd523069350776
Yap Cheah Shen <yapcheahshen@gmail.com>
2013-08-29 22:52:03 +0800

    fix wrong filename

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var yase=require('./yase'); // do not invoke with new
 var Yasew=require('./yasew');
 var Yasebuild=require('./yasebuild');
 var splitter=require('./splitter');
-var splitter2=require('./splitter2');
+var tokenize=require('./tokenize');
 var customfunc=require('./yasecustom');
 var api=require('./yase_api');
 var schema=require('./schema');
 var Genschema=require('./genschema');
 var version=require('./package.json').version;
 module.exports={use:yase, create: Yasew, build: Yasebuild,
-	customfunc:customfunc, schema:schema, splitter:splitter, splitter2:splitter2,
+	customfunc:customfunc, schema:schema, splitter:splitter, tokenize:tokenize,
 	 api: api, Genschema: Genschema, version:version};
